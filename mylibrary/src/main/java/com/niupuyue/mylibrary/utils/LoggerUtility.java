a4981bde9435ae556753209d43247eab72d8b9e9
paulniu <niupuyue@aliyun.com>
2019-06-15 22:51:43 +0800

    add logger method

--- mylibrary/src/main/java/com/niupuyue/mylibrary/utils/LoggerUtility.java
+++ mylibrary/src/main/java/com/niupuyue/mylibrary/utils/LoggerUtility.java
@@ -1,5 +1,7 @@
 package com.niupuyue.mylibrary.utils;
 
+import android.util.Log;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,39 +16,72 @@ import java.util.Set;
 public class LoggerUtility {
 
     public static void e(String tag, String value) {
-
+        if (BaseUtility.isEmpty(tag) || BaseUtility.isEmpty(value)) return;
+        Log.e(tag, value);
     }
 
     public static void e(String tag, int value) {
-
+        if (BaseUtility.isEmpty(tag)) return;
+        Log.e(tag, String.valueOf(value));
     }
 
     public static void e(String tag, boolean value) {
-
+        if (BaseUtility.isEmpty(tag)) return;
+        Log.e(tag, String.valueOf(value));
     }
 
     public static void e(String tag, long value) {
-
+        if (BaseUtility.isEmpty(tag)) return;
+        Log.e(tag, String.valueOf(value));
     }
 
     public static void e(String tag, List<?> values) {
-
+        if (BaseUtility.isEmpty(values) || BaseUtility.isEmpty(tag)) return;
+        Log.e(tag, values.toString());
     }
 
     public static void e(String tag, Object[] objects) {
-
+        if (BaseUtility.isEmpty(tag) || BaseUtility.isEmpty(objects)) return;
+        String res = "[";
+        for (Object obj : objects) {
+            if (obj != null) {
+                res += obj.toString();
+                res += ";";
+            }
+        }
+        res += "]";
+        Log.e(tag, res);
     }
 
     public static void e(String tag, Set<?> values) {
-
+        if (BaseUtility.isEmpty(tag) || BaseUtility.isEmpty(values)) return;
+        String res = "[";
+        for (Object obj : values) {
+            if (obj != null) {
+                res += obj.toString();
+                res += ";";
+            }
+        }
+        res += "]";
+        Log.e(tag, res);
     }
 
     public static void e(String tag, Map<?, ?> values) {
-
+        if (BaseUtility.isEmpty(tag) || BaseUtility.isEmpty(values)) return;
+        String res = "{ ";
+        for (Object key : values.keySet()) {
+            if (key != null) {
+                Object value = values.get(key);
+                res = res + " [ " + key + ":" + value + " ],";
+            }
+        }
+        res += " }";
+        Log.e(tag, res);
     }
 
     public static void e(String tag, Object object) {
-
+        if (BaseUtility.isEmpty(tag) || null == object) return;
+        Log.e(tag, object.toString());
     }
 
     public static void e(String value) {
