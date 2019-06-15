0f8ba1ad291db38f5a60e29bf0e87ddbbd9c08a6
paulniu <niupuyue@aliyun.com>
2019-06-15 22:35:09 +0800

    add logger

---
+++ mylibrary/src/main/java/com/niupuyue/mylibrary/utils/LoggerUtility.java
@@ -0,0 +1,87 @@
+package com.niupuyue.mylibrary.utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Coder: niupuyue (牛谱乐)
+ * Date: 2019/6/15
+ * Time: 22:29
+ * Desc: Logger 日志
+ * Version:
+ */
+public class LoggerUtility {
+
+    public static void e(String tag, String value) {
+
+    }
+
+    public static void e(String tag, int value) {
+
+    }
+
+    public static void e(String tag, boolean value) {
+
+    }
+
+    public static void e(String tag, long value) {
+
+    }
+
+    public static void e(String tag, List<?> values) {
+
+    }
+
+    public static void e(String tag, Object[] objects) {
+
+    }
+
+    public static void e(String tag, Set<?> values) {
+
+    }
+
+    public static void e(String tag, Map<?, ?> values) {
+
+    }
+
+    public static void e(String tag, Object object) {
+
+    }
+
+    public static void e(String value) {
+
+    }
+
+    public static void e(boolean value) {
+
+    }
+
+    public static void e(int value) {
+
+    }
+
+    public static void e(long value) {
+
+    }
+
+    public static void e(List<?> values) {
+
+    }
+
+    public static void e(Set<?> values) {
+
+    }
+
+    public static void e(Map<?, ?> values) {
+
+    }
+
+    public static void e(Object[] objects) {
+
+    }
+
+    public static void e(Object object) {
+
+    }
+}
