4a2a034b0d2772fabfc17f6bc4dfbd6ad0cb62c0
paulniu <niupuyue@aliyun.com>
2019-06-21 23:28:50 +0800

    add notification message

--- mylibrary/src/main/java/com/niupuyue/mylibrary/utils/NotificationUtility.java
+++ mylibrary/src/main/java/com/niupuyue/mylibrary/utils/NotificationUtility.java
@@ -7,8 +7,12 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.os.Handler;
 import android.support.annotation.RequiresApi;
+
+import com.niupuyue.mylibrary.R;
 
 
 /**
@@ -16,13 +20,17 @@ import android.support.annotation.RequiresApi;
  */
 public class NotificationUtility extends ContextWrapper {
 
-    private static Context mContext;
     private NotificationManager notificationManager;
     private Notification.Builder notification;
-
     public static String NOTIFICATION_STATIC_ID = "1001";
     private static String NOTIFICATION_STATIC_NAME = "新消息通知";
     private static int NOTIFICATION_CHANNEL_ID = 0x100;
+
+    private String title;
+    private String content;
+    private int icon;
+    private PendingIntent pendingIntent;
+    private int textColor;
 
     private NotificationUtility(Context context) {
         super(context);
@@ -35,44 +43,69 @@ public class NotificationUtility extends ContextWrapper {
     /**
      * 判断当前SDK是否大于26
      *
-     * @return
+     * @return true 大于26
      */
     private boolean checkSdkVersionMoreO() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
     }
 
     /**
+     * 设置是否给出提示音和震动效果
+     */
+    private int setSoundAndVibrate() {
+        int default_setting;
+        // 根据设置决定是否给出提示音和震动效果 默认添加亮屏操作
+        default_setting = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS;
+        return default_setting;
+    }
+
+    /**
      * 创建Notification对象，针对Android8.0以下
      */
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    private void createPushNotificationAfter3(String title, String content, Intent intent, int icon) {
-        if (intent == null) return;
+    private void createPushNotificationAfter3(final String content, final String title, final int icon, final PendingIntent pendingIntent) {
         try {
             if (null == notificationManager) {
-                notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager = (NotificationManager) LibraryConstants.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
             }
-            notification = new Notification.Builder(mContext)
-                    .setSmallIcon(icon)
-                    .setContentTitle(title)
-                    .setContentText(content)
-                    .setPriority(Notification.PRIORITY_HIGH)
-                    .setWhen(System.currentTimeMillis())
-                    .setContentIntent(initJumpAction(intent))
-                    .setOngoing(false)
-                    .setAutoCancel(true);
-            int default_setting = 0;
-            // 根据设置决定是否给出提示音和震动效果
-//            if (EditSharedPreferences.getNotifySound() && EditSharedPreferences.getNotifyVibrator()) {
-//                default_setting = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE;
-//            } else if (EditSharedPreferences.getNotifySound() && !EditSharedPreferences.getNotifyVibrator()) {
-//                default_setting = Notification.DEFAULT_SOUND;
-//            } else if (EditSharedPreferences.getNotifyVibrator() && !EditSharedPreferences.getNotifySound()) {
-//                default_setting = Notification.DEFAULT_VIBRATE;
-//            } else if (!EditSharedPreferences.getNotifySound() && !EditSharedPreferences.getNotifyVibrator()) {
-//                default_setting = 0;
-//            }
-            notification.setDefaults(default_setting);
+            notification = new Notification.Builder(LibraryConstants.getContext());
+            notification.setSmallIcon(icon);
+            notification.setLargeIcon(BitmapFactory.decodeResource(LibraryConstants.getContext().getResources(), icon));
+            notification.setContentTitle(BaseUtility.isEmpty(title) ? LibraryConstants.getContext().getString(R.string.app_name) : title);
+            notification.setContentText(content);
+            notification.setPriority(Notification.PRIORITY_HIGH);
+            notification.setWhen(System.currentTimeMillis());
+            notification.setContentIntent(pendingIntent);
+            notification.setAutoCancel(true);
+            notification.setDefaults(setSoundAndVibrate());
+            // 小米手机不要使用下面的方式设置，会直接执行intent
+            if (AndroidUtility.PhoneType.Xiaomi != AndroidUtility.getPhoneType() && AndroidUtility.PhoneType.Meizu != AndroidUtility.getPhoneType()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    notification.setVisibility(Notification.VISIBILITY_PUBLIC);
+                    // 关联PendingIntent
+                    notification.setFullScreenIntent(pendingIntent, true);
+                }
+            }
             notificationManager.notify(NOTIFICATION_CHANNEL_ID, notification.build());
+            // 如果是vivo手机，3秒之后关闭弹窗
+            if (AndroidUtility.getPhoneType() == AndroidUtility.PhoneType.Vivo) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        notificationManager.cancel(NOTIFICATION_CHANNEL_ID);
+                        notification = new Notification.Builder(LibraryConstants.getContext());
+                        notification.setSmallIcon(icon);
+                        notification.setLargeIcon(BitmapFactory.decodeResource(LibraryConstants.getContext().getResources(), icon));
+                        notification.setContentTitle(BaseUtility.isEmpty(title) ? LibraryConstants.getContext().getString(R.string.app_name) : title);
+                        notification.setContentText(content);
+                        notification.setPriority(Notification.PRIORITY_HIGH);
+                        notification.setWhen(System.currentTimeMillis());
+                        notification.setContentIntent(pendingIntent);
+                        notification.setAutoCancel(true);
+                        notificationManager.notify(NOTIFICATION_CHANNEL_ID, notification.build());
+                    }
+                }, 3000);
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -82,23 +115,23 @@ public class NotificationUtility extends ContextWrapper {
      * 创建Notification对象，针对Android8.0以上
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
-    private void createPushNotificationAfter8(String title, String content, Intent intent, int icon) {
-        if (intent == null) return;
+    private void createPushNotificationAfter8(String content, String title, int icon, PendingIntent pendingIntent) {
         try {
             if (null == notificationManager) {
-                notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager = (NotificationManager) LibraryConstants.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
             }
-            NotificationChannel mChannel = new NotificationChannel(NOTIFICATION_STATIC_ID, NOTIFICATION_STATIC_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel mChannel = new NotificationChannel(NOTIFICATION_STATIC_ID, NOTIFICATION_STATIC_NAME, NotificationManager.IMPORTANCE_HIGH);
             notificationManager.createNotificationChannel(mChannel);
-            notification = new Notification.Builder(mContext, NOTIFICATION_STATIC_ID)
-                    .setContentTitle(title)
+            notification = new Notification.Builder(LibraryConstants.getContext(), NOTIFICATION_STATIC_ID)
+                    .setContentTitle(BaseUtility.isEmpty(title) ? LibraryConstants.getContext().getString(R.string.app_name) : title)
                     .setContentText(content)
                     .setSmallIcon(icon)
+//                    .setColor()
                     .setWhen(System.currentTimeMillis())
                     .setAutoCancel(true)
                     .setPriority(Notification.PRIORITY_HIGH)
                     .setOngoing(false)
-                    .setContentIntent(initJumpAction(intent))
+                    .setContentIntent(pendingIntent)
                     .setDefaults(Notification.DEFAULT_ALL);
             //发送
             notificationManager.notify(NOTIFICATION_CHANNEL_ID, notification.build());
@@ -113,11 +146,11 @@ public class NotificationUtility extends ContextWrapper {
      * @param content 通知栏中的content
      */
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    private void showPushNotification(String title, String content, Intent intent, int icon) {
+    private void showPushNotification(String content, String title, int icon, PendingIntent pendingIntent) {
         if (checkSdkVersionMoreO()) {
-            createPushNotificationAfter8(title, content, intent, icon);
+            createPushNotificationAfter8(content, title, icon, pendingIntent);
         } else {
-            createPushNotificationAfter3(title, content, intent, icon);
+            createPushNotificationAfter3(content, title, icon, pendingIntent);
         }
     }
 
@@ -131,40 +164,50 @@ public class NotificationUtility extends ContextWrapper {
     }
 
     /**
-     * 初始化跳转PendingIntent
+     * 返回通知栏对象
      */
-    private PendingIntent initJumpAction(Intent intent) {
-        if (intent == null) return null;
-        try {
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);           //添加为栈顶Activity
-            PendingIntent resultPendingIntent = PendingIntent.getActivity(mContext, -1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            return resultPendingIntent;
-        } catch (Exception ex) {
-            ex.printStackTrace();
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Notification getNotification() {
+        if (null == notificationManager) {
+            notificationManager = (NotificationManager) LibraryConstants.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
         }
-        return null;
-    }
-
-    /**
-     * 添加通知栏消息 这里可以对标题和内容进行格式化
-     */
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    public void addPushNotification(String title, String content, Intent intent, int icon) {
-        showPushNotification(title, content, intent, icon);
+        if (notification != null) return notification.build();
+        if (checkSdkVersionMoreO()) {
+            NotificationChannel mChannel = new NotificationChannel(NOTIFICATION_STATIC_ID, NOTIFICATION_STATIC_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(mChannel);
+            notification = new Notification.Builder(LibraryConstants.getContext(), NOTIFICATION_STATIC_ID)
+                    .setContentTitle(LibraryConstants.getContext().getString(R.string.app_name))
+                    .setContentText(null)
+                    .setWhen(System.currentTimeMillis())
+                    .setAutoCancel(true)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setOngoing(false)
+                    .setDefaults(Notification.DEFAULT_ALL);
+        } else {
+            notification = new Notification.Builder(LibraryConstants.getContext())
+                    .setSmallIcon(icon)
+                    .setContentTitle(BaseUtility.isEmpty(title) ? LibraryConstants.getContext().getString(R.string.app_name) : title)
+                    .setContentText(content)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setWhen(System.currentTimeMillis())
+                    .setOngoing(false)
+                    .setAutoCancel(true);
+        }
+        return notification.build();
     }
 
     /**
      * ///////////////////////////////////////////////////////匿名内部类构造单例模式 开始//////////////////////////////////////////////////////////////////
      */
-    public static NotificationUtility getInstance(Context context) {
-        mContext = context;
+    public static NotificationUtility getInstance() {
         return PushNotificationManagerHolder.instance;
     }
 
     private static class PushNotificationManagerHolder {
-        private static final NotificationUtility instance = new NotificationUtility(mContext);
+        private static final NotificationUtility instance = new NotificationUtility(LibraryConstants.getContext());
     }
     /**
      * ///////////////////////////////////////////////////////匿名内部类构造单例模式 结束//////////////////////////////////////////////////////////////////
      */
+
 }
