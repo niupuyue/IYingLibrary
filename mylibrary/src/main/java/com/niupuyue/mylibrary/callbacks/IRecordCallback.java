fd9d2db0df1e4bd381f9e76047e3d019b1a11af9
paulniu <niupuyue@aliyun.com>
2019-07-08 23:03:52 +0800

    add vocie record utility

---
+++ mylibrary/src/main/java/com/niupuyue/mylibrary/callbacks/IRecordCallback.java
@@ -0,0 +1,35 @@
+package com.niupuyue.mylibrary.callbacks;
+
+/**
+ * Coder: niupuyue (牛谱乐)
+ * Date: 2019-07-08
+ * Time: 22:54
+ * Desc: 语音录制接口回调
+ * Version:
+ */
+public interface IRecordCallback {
+
+    /**
+     * 录音成功
+     *
+     * @param var1 var1 数量存在多个，其中第一个和第二个是文件缓存路径和录音时长
+     */
+    void onSuccess(Object... var1);
+
+    /**
+     * 语音错误
+     *
+     * @param errorCode 错误代码
+     * @param errorMsg  错误信息
+     */
+    void onError(int errorCode, String errorMsg);
+
+    /**
+     * 正在录音
+     *
+     * @param timeCount 时间计数
+     */
+    void onProgress(int timeCount);
+
+
+}
