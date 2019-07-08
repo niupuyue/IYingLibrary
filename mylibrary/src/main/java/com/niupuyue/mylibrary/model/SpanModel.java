57d89119b78f62e4e3a633416ce7a24822c3ba6f
paulniu <niupuyue@aliyun.com>
2019-07-08 22:31:19 +0800

    add String operation security function

---
+++ mylibrary/src/main/java/com/niupuyue/mylibrary/model/SpanModel.java
@@ -0,0 +1,33 @@
+package com.niupuyue.mylibrary.model;
+
+/**
+ * Coder: niupuyue (牛谱乐)
+ * Date: 2019-07-08
+ * Time: 22:25
+ * Desc: 设置SpanableStringBuilder
+ * Version:
+ */
+public class SpanModel {
+
+    /**
+     * 文字
+     */
+    public String text;
+    /**
+     * text 文字颜色
+     */
+    public int color;
+    /**
+     * text 是否下划线
+     */
+    public boolean isUnderline;
+    /**
+     * text是否加粗
+     */
+    public boolean isBold;
+    /**
+     * 字体大小，大于 0 有效
+     */
+    public float textSize;
+
+}
---
+++ mylibrary/src/main/java/com/niupuyue/mylibrary/utils/SpanStringUtility.java
@@ -0,0 +1,175 @@
+package com.niupuyue.mylibrary.utils;
+
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import com.niupuyue.mylibrary.callbacks.ITextviewClickable;
+import com.niupuyue.mylibrary.model.SpanModel;
+import com.niupuyue.mylibrary.widgets.SpanClickable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Coder: niupuyue (牛谱乐)
+ * Date: 2019-07-08
+ * Time: 22:24
+ * Desc: 字符串相关工具类
+ * Version:
+ */
+public class SpanStringUtility {
+
+    /**
+     * 可同时设置字体颜色、是否下划线、是否加粗、文字大小和点击事件
+     *
+     * @param tv           TextView
+     * @param text         要显示的全部文本
+     * @param hotWordModel @see SpanModel
+     * @param listener     点击监听
+     */
+    public static void setSpanText(TextView tv, CharSequence text, List<SpanModel> hotWordModel, ITextviewClickable listener) {
+        if (tv == null || BaseUtility.isEmpty(text) || BaseUtility.isEmpty(hotWordModel)) {
+            return;
+        }
+        try {
+            SpannableStringBuilder textBuilder = new SpannableStringBuilder(text);
+
+            List<Integer> lis;
+            for (int i = 0; i < hotWordModel.size(); i++) {
+                lis = getStrIndex(text + "", hotWordModel.get(i).text);
+                for (int j = 0; j < lis.size(); j++) {
+                    textBuilder.setSpan(new SpanClickable(listener, i, hotWordModel.get(i).isUnderline, hotWordModel.get(i).color, hotWordModel.get(i).isBold, hotWordModel.get(i).textSize),
+                            lis.get(j),
+                            lis.get(j) + hotWordModel.get(i).text.length(),
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
+            }
+            tv.setMovementMethod(LinkMovementMethod.getInstance());
+            tv.setHighlightColor(Color.TRANSPARENT);
+            tv.setText(textBuilder);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * @param tv
+     * @param text            Text
+     * @param arrHotWords     关键词
+     * @param colorResourceID 关键词颜色
+     * @param isBold          关键词是否加粗
+     * @param textSize        关键字文字大小
+     * @param listener        关键字点击回调
+     */
+    public static void setHotWordsText(TextView tv, CharSequence text, String[] arrHotWords, int colorResourceID, boolean isBold, float textSize, ITextviewClickable listener) {
+        if (tv == null || BaseUtility.isEmpty(text) || BaseUtility.isEmpty(arrHotWords)) {
+            return;
+        }
+        try {
+            List<SpanModel> hotWordModels = new ArrayList<>();
+            SpanModel spanModel;
+            for (String arrHotWord : arrHotWords) {
+                spanModel = new SpanModel();
+                spanModel.text = arrHotWord;
+                spanModel.isUnderline = false;
+                spanModel.color = ContextCompat.getColor(LibraryConstants.getContext(), colorResourceID);
+                spanModel.isBold = isBold;
+                spanModel.textSize = textSize;
+                hotWordModels.add(spanModel);
+            }
+            setSpanText(tv, text, hotWordModels, listener);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param tv
+     * @param text            Text
+     * @param arrHotWords     关键词
+     * @param colorResourceID 关键词颜色
+     * @param isBold          关键词是否加粗
+     * @param listener        关键字点击回调
+     */
+    public static void setHotWordsText(TextView tv, CharSequence text, String[] arrHotWords, int colorResourceID, boolean isBold, ITextviewClickable listener) {
+        setHotWordsText(tv, text, arrHotWords, colorResourceID, isBold, -1, listener);
+    }
+
+    /**
+     * @param tv
+     * @param text            Text
+     * @param arrHotWords     关键词
+     * @param colorResourceID 关键词颜色
+     * @param listener        关键字点击回调
+     */
+    public static void setHotWordsText(TextView tv, CharSequence text, String[] arrHotWords, int colorResourceID, ITextviewClickable listener) {
+        setHotWordsText(tv, text, arrHotWords, colorResourceID, false, listener);
+    }
+
+    /**
+     * @param tv
+     * @param text            Text
+     * @param hotWords        关键词
+     * @param colorResourceID 关键词颜色
+     * @param listener        关键字点击回调
+     */
+    public static void setHotWordsText(TextView tv, CharSequence text, String hotWords, int colorResourceID, ITextviewClickable listener) {
+        setHotWordsText(tv, text, new String[]{hotWords}, colorResourceID, listener);
+    }
+
+    /**
+     * @param tv
+     * @param text            Text
+     * @param hotWords        关键词
+     * @param colorResourceID 关键词颜色
+     */
+    public static void setHotWordsText(TextView tv, CharSequence text, String hotWords, int colorResourceID) {
+        setHotWordsText(tv, text, hotWords, colorResourceID, null);
+    }
+
+    /**
+     * @param tv
+     * @param text            Text
+     * @param arrHotWords     关键词
+     * @param colorResourceID 关键词颜色
+     */
+    public static void setHotWordsText(TextView tv, CharSequence text, String[] arrHotWords, int colorResourceID) {
+        setHotWordsText(tv, text, arrHotWords, colorResourceID, null);
+    }
+
+    /**
+     * 获取热词在字符串中位置的集合(忽略大小写)
+     *
+     * @param source
+     * @param hotWord
+     * @return
+     */
+    public static List<Integer> getStrIndex(String source, String hotWord) {
+        List<Integer> lis = new ArrayList<>();
+
+        // 忽略大小写
+        final String tempLowerCaseStr = source.toLowerCase();
+        final String tempLowerHotWord = hotWord.toLowerCase();
+
+        int indexOf = tempLowerCaseStr.indexOf(tempLowerHotWord);
+        if (indexOf != -1) {
+            lis.add(indexOf);
+        }
+
+        while (indexOf != -1) {
+
+            indexOf = tempLowerCaseStr.indexOf(tempLowerHotWord, indexOf + 1);
+
+            if (indexOf != -1) {
+                lis.add(indexOf);
+            }
+        }
+
+        return lis;
+    }
+
+}
