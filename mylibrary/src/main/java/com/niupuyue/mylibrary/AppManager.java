561bfed554cc327a91b8001b3bce88f92dea55e2
paulniu <niupuyue@aliyun.com>
2019-07-09 23:26:41 +0800

    add activity manager some function

--- mylibrary/src/main/java/com/niupuyue/mylibrary/AppManager.java
+++ mylibrary/src/main/java/com/niupuyue/mylibrary/AppManager.java
@@ -1,5 +1,7 @@
 package com.niupuyue.mylibrary;
 
+import android.app.Activity;
+
 /**
  * Coder: niupuyue (牛谱乐)
  * Date: 2019-07-09
@@ -8,4 +10,60 @@ package com.niupuyue.mylibrary;
  * Version:
  */
 public class AppManager {
+
+    /**
+     * 添加Activity
+     */
+    public void addActivity(Activity activity) {
+
+    }
+
+    public void addActivity(Class<Activity> cls) {
+
+    }
+
+    /**
+     * 移除Activity
+     */
+    public void removeActivity(Activity activity) {
+
+    }
+
+    public void removeActivity(Class<Activity> cls) {
+
+    }
+
+    /**
+     * 判断Activity是否存在于集合中
+     */
+    public boolean isExistActivity(Activity activity) {
+        boolean isExist = false;
+
+        return isExist;
+    }
+
+    public boolean isExistActivity(Class<Activity> cls) {
+        boolean isExist = false;
+
+        return isExist;
+    }
+
+    /**
+     * 获取栈顶对象
+     *
+     * @return
+     */
+    public Activity getTopActivity() {
+        Activity topActivity = null;
+
+        return topActivity;
+    }
+
+    /**
+     * 移除所有的Activity
+     */
+    public void removeAll() {
+
+    }
+
 }
