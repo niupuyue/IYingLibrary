2c625deefcf45d52a2bb29d5631d7fa4db0b88ca
paulniu <niupuyue@aliyun.com>
2019-07-10 09:43:17 +0800

    fix bug

--- app/src/main/java/com/niupuyue/iyinglibrary/MainActivity.java
+++ app/src/main/java/com/niupuyue/iyinglibrary/MainActivity.java
@@ -6,8 +6,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.niupuyue.mylibrary.widgets.filepicker.IYFilePicker;
-import com.niupuyue.mylibrary.widgets.filepicker.callback.IYFilePickerCallback;
 
 import java.util.List;
 
@@ -21,21 +19,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn01 = findViewById(R.id.btn01);
-        btn01.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new IYFilePicker().with(MainActivity.this)
-                        .withFileFilter(new String[]{"doc", "docx"})
-                        .withMutilyMode(false)
-                        .withTitle("文件遍历")
-                        .withFilePickerCallback(new IYFilePickerCallback() {
-                            @Override
-                            public void onChooseFiles(List<String> filePathes) {
-                                Log.e("npl", filePathes.toString());
-                            }
-                        })
-                        .start();
-            }
-        });
     }
 }
